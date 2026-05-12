bfb07d81b617aa279d6b10f9b20c6b130199ba30
hh16commits <hh1676443@gmail.com>
2026-05-12 14:38:56 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
