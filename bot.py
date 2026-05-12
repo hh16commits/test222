b3e6b2b912c7e6d24162462bda297d31933b9167
hh16commits <hh1676443@gmail.com>
2026-05-12 14:55:16 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,45 +1,83 @@
 import os
-import google.generativeai as genai
+from telegram import (
+    Update,
+    ReplyKeyboardMarkup
+)
 
-from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
+    CommandHandler,
     MessageHandler,
     ContextTypes,
     filters
 )
 
 TOKEN = os.getenv("TOKEN")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-
-genai.configure(api_key=GEMINI_API_KEY)
-
-model = genai.GenerativeModel("gemini-flash-latest")
 
 
-async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_message = update.message.text
+keyboard = [
+    ["🛍 Каталог", "📦 Заказать"],
+    ["💬 Поддержка", "📍 О нас"]
+]
 
-    try:
-        response = model.generate_content(user_message)
+reply_markup = ReplyKeyboardMarkup(
+    keyboard,
+    resize_keyboard=True
+)
 
-        answer = response.text
 
-        if len(answer) > 4000:
-            answer = answer[:4000]
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = (
+        "✨ Добро пожаловать в GlowRush!\n\n"
+        "Корейская косметика 🇰🇷"
+    )
 
-        await update.message.reply_text(answer)
+    await update.message.reply_text(
+        text,
+        reply_markup=reply_markup
+    )
 
-    except Exception as e:
-        await update.message.reply_text(f"Ошибка: {e}")
+
+async def messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+
+    if text == "🛍 Каталог":
+        await update.message.reply_text(
+            "Каталог скоро будет доступен 🔥"
+        )
+
+    elif text == "📦 Заказать":
+        await update.message.reply_text(
+            "Напишите название товара ✍️"
+        )
+
+    elif text == "💬 Поддержка":
+        await update.message.reply_text(
+            "Поддержка: @your_username"
+        )
+
+    elif text == "📍 О нас":
+        await update.message.reply_text(
+            "GlowRush — магазин корейской косметики 🇰🇷"
+        )
+
+    else:
+        await update.message.reply_text(
+            "Выберите кнопку 👇"
+        )
 
 
 app = ApplicationBuilder().token(TOKEN).build()
 
+app.add_handler(CommandHandler("start", start))
+
 app.add_handler(
-    MessageHandler(filters.TEXT & ~filters.COMMAND, chat)
+    MessageHandler(
+        filters.TEXT & ~filters.COMMAND,
+        messages
+    )
 )
 
-print("AI bot started 🚀")
+print("Business bot started 🚀")
 
 app.run_polling(close_loop=False)
